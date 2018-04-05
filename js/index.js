3f967e9f27f968f8cbf9a671a54044ed1f6b257e
Kalia Hayes <kaliahayes@yahoo.com>
2018-04-05 15:25:07 -0500

    Added Code

---
+++ js/index.js
@@ -0,0 +1,18 @@
+/*
+Since IDK js yet, I'll walkthrough what it is I'm doing.
+
+HEADER
+	I made the width auto so that ti would reach end to endon the page. I also made the margin 0 auto just because. I put in a nice header height. To make it reach the edges of the page on all sides, in the body style i made the margin 0 auto.
+
+HEADER BUTTONS	
+	To achieve the buttons, I created a class called hButtons and placed it inside the header tag. I added a padding of 9 and a padding left of 200 so that they'd be 200px inside the header. I found that padding was essential or else there'd be white space above the header.
+	I made an unordered list and each point was a button title. I styled the list so that the display was inline andthere was a margin of 5px between each button. The font was raleway and it was bold and capsed. On hover, the button text turned from white to a blue and the background became a slightly light shade of dark grey.
+	
+IMAGE AREA
+	I found the URL for the picture,	added it in, set a height of 800 and a width of 100 with auto margins. Also I set the height of the content to 800 also. I'm not sure if these are relevant yet, but I'll find out.
+	
+MAIN AREA	
+	In my content div, I put two other divs, title and cButton. In order to get my title centered, I did text-align:center. In order to get my button centered, I had to do margin:0 auto; bc the text only centered with the div not the page. ALSO, in order to have the backgroudn of the button show, I had to make the position relative, which I think made it go ontop the BG and right under the title. Seems like some sort of smart tool or whatever idk. I only say ths bc I didn't have to set the margin top negative like I did for the title. I think it knew to get right under the title with position:relative.
+	
+
+*/
